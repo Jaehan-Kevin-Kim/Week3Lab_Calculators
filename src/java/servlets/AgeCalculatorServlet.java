c217daf96de04609464b858cd5c16a4c8bd0ec8a
841898 <841898@SAIT225741IT.ACDM.DS.SAIT.CA>
2021-01-27 09:29:10 -0700

    Add validation Added validation to the form to ensure that user typed in age and the user typed the number

--- src/java/servlets/AgeCalculatorServlet.java
+++ src/java/servlets/AgeCalculatorServlet.java
@@ -1,4 +1,3 @@
-
 package servlets;
 
 import java.io.IOException;
@@ -9,29 +8,37 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-
 @WebServlet(name = "AgeCalculatorServlet", urlPatterns = {"/age"})
 public class AgeCalculatorServlet extends HttpServlet {
-
-
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-          getServletContext().getRequestDispatcher("/WEB-INF/agecalculator.jsp").forward(request, response);
+        getServletContext().getRequestDispatcher("/WEB-INF/agecalculator.jsp").forward(request, response);
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        
-          int currentAge = Integer.parseInt((request.getParameter("age")));
-            String nextAge = Integer.toString(++currentAge);
-              
-        
+
+        String currentAgeString = request.getParameter("age");
+
+        if (currentAgeString == null || currentAgeString.equals("")) {
+            request.setAttribute("message", "You must give your current age");
+
+        } else {
+            try {
+                int currentAgeInt = Integer.parseInt((request.getParameter("age")));
+                String nextAge = Integer.toString(++currentAgeInt);
+                request.setAttribute("message", "Your age next birthday will be " + nextAge);
+            } catch (Exception e) {
+                request.setAttribute("message", "You must enter a number.");
+
+            }
+        }
+
 //        request.setAttribute("message", "Your age next birthday will be "+ nextAge);
-        request.setAttribute("message",  "Your age next birthday will be "+ nextAge);
-           getServletContext().getRequestDispatcher("/WEB-INF/agecalculator.jsp").forward(request, response);
+        getServletContext().getRequestDispatcher("/WEB-INF/agecalculator.jsp").forward(request, response);
 //        getServletContext().getRequestDispatcher("/WEB-INF/agecalculator.jsp").forward(request, response);
     }
 
